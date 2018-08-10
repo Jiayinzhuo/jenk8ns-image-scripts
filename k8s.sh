59fc7a82c97bfbde51d5a4c8a2bdd900b147285e
Thegaijin <ssemagandavictor@gmail.com>
2018-08-10 21:14:45 +0300

    add pem and change how cluster is created

--- k8s.sh
+++ k8s.sh
@@ -1,13 +1,17 @@
 . /home/ubuntu/.env
 
 echo "Get s3 bucket..."
-## creating cluster state storage
+# Check available buckets
 buckets="$(aws s3api list-buckets | jq -r '.Buckets')"
 found_bucket=false
+
+# check if bucket already exists
 for name in $( echo ${buckets} | jq -c '.[]'); do
-        bucket_name=$(echo ${name} | jq -r '.Name')
-        if [ ${bucket_name} == ${BUCKET_NAME} ]; then
-		found_bucket=true
+	bucket_names=$(echo ${name} | jq -r '.Name')
+	the_bucket=$(echo ${bucket_names} | grep ${BUCKET_NAME})
+	if [[ ${the_bucket} == ${BUCKET_NAME} ]]; then
+	found_bucket=true
+	break
 	fi
 done
 
@@ -15,7 +19,7 @@ if [ ${found_bucket} == false ]; then
 	echo "Create s3 bucket..."
 	export BUCKET_NAME=k8s-$(date +%s)
 	echo $BUCKET_NAME
-	aws s3api create-bucket --bucket $BUCKET_NAME --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION
+	aws s3api create-bucket --bucket $BUCKET_NAME
 	export KOPS_STATE_STORE=s3://$BUCKET_NAME
 else
 	echo "Using existing s3 bucket..."
@@ -23,7 +27,6 @@ fi
 
 echo "Creating cluster..."
 # creating a cluster
-kops create cluster --name $NAME --master-count 1 --master-size t2.micro --node-count 4 --node-size t2.micro --zones $ZONE --master-zones $ZONE --ssh-public-key kube-key.pub --yes
 kops create cluster --dns-zone thegaijin.xyz --zones us-east-1a --master-size t2.micro --node-size t2.micro --name cluster.thegaijin.xyz --yes
 while true; do
   kops validate cluster --name $NAME | grep 'is ready' &> /dev/null
