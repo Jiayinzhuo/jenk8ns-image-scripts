c18c56b828bc53903dc435535736f253a4971df5
Thegaijin <ssemagandavictor@gmail.com>
2018-08-12 03:59:36 +0300

    Trail and error continues to solve certificate issue

--- k8s.sh
+++ k8s.sh
@@ -25,22 +25,56 @@ else
 	echo "Using existing s3 bucket..."
 fi
 
+# delete key-pair to avoid name conflicts
+aws ec2 delete-key-pair --key-name /home/ubuntu/${KEY_NAME}
+echo "Nasiiimmmmwwwweeeeeennnnnnyyyyyyyyaaaaaaaa"
+ls -la
+echo "saywhat"
+ls -la /home/ubuntu
+
+# create a new pem file
+aws ec2 create-key-pair --key-name /home/ubuntu/${KEY_NAME} | jq -r '.KeyMaterial' > /home/ubuntu/k8s-key.pem
+cat /home/ubuntu/k8s-key.pem
+echo "Lion King"
+ls -la /home/ubuntu/k8s-key.pem
+
+chmod 400 /home/ubuntu/k8s-key.pem
+echo "King of the jungle"
+ls -la /home/ubuntu/k8s-key.pem
+
+# create a public key
+ssh-keygen -y -f /home/ubuntu/k8s-key.pem > /home/ubuntu/.ssh/id_rsa.pub
+ls -la /home/ubuntu/.ssh/id_rsa.pub
+cat /home/ubuntu/.ssh/id_rsa.pub
+
 echo "Creating cluster..."
 # creating a cluster
-kops create cluster --dns-zone thegaijin.xyz --zones us-east-1a --master-size t2.micro --node-size t2.micro --name $NAME --ssh-public-key /home/ubuntu/id_rsa.pub --yes
+current=$(eval whoami)
+echo $current
+kops create cluster --dns-zone thegaijin.xyz --zones us-east-1a --master-size t2.micro --node-size t2.micro --name $CLUSTER_NAME --ssh-public-key /home/ubuntu/.ssh/id_rsa.pub --yes
+
+# echo "Kubectil certificate issue"
+# kops export kubecfg --name $CLUSTER_NAME
+echo "Do the .kube ting"
+mkdir -p /.kube
+sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+sudo chown $(id -u):$(id -g) /home/ubuntu/.kube/config
+
+echo "************************ validate cluster **************************"
 while true; do
-  kops validate cluster --name $NAME | grep 'is ready' &> /dev/null
+  kops validate cluster --name $CLUSTER_NAME | grep 'is ready' &> /dev/null
   if [ $? == 0 ]; then
      break
   fi
     sleep 30
 done
 
+echo "<<<<<<<<<<<<< get the cluster >>>>>>>>>>>>>"
 kops get cluster
 kubectl cluster-info
 
 
-#kops validate cluster
+echo "Jenkins shit"
 sudo mkdir -p /var/lib/jenkins/.kube
 sudo cp ~/.kube/config /var/lib/jenkins/.kube/
 cd /var/lib/jenkins/.kube/
