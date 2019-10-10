3301a1c5a7b752eea8321c8f73ea902f79453f81
Jiayin Zhuo <jiayin.zhuo@gmail.com>
2019-10-10 17:10:31 -0400

    update again

--- k8s.sh
+++ k8s.sh
@@ -46,9 +46,13 @@ kubectl cluster-info
 
 echo "Add Dashboard and User"
 #kubectl create -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.4.0.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
-kubectl apply -f https://raw.githubusercontent.com/Jiayinzhuo/jenk8ns-image-scripts/master/dashboard-adminuser.yaml
-kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+#kubectl create secret generic kubernetes-dashboard-certs --from-file=$HOME/certs -n kubernetes-dashboard
+#kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/alternative.yaml
+
+#kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
+#kubectl apply -f https://raw.githubusercontent.com/Jiayinzhuo/jenk8ns-image-scripts/master/dashboard-adminuser.yaml
+#kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
 
 echo "Add ingress"
 kubectl create namespace ingress-nginx
@@ -73,11 +77,14 @@ sudo chown jenkins:jenkins config
 sudo chmod 750 config
 
 echo "<<<<<<<<<<<<< get the cluster again >>>>>>>>>>>>>"
-kubectl cluster-info
-kubectl get nodes
-kubectl get services
+#kubectl get nodes
+#kubectl get services
+echo "kubectl get deployments --all-namespaces"
 kubectl get deployments --all-namespaces
-kubectl -n kube-system get po
+#kubectl -n kube-system get po
+echo "kubectl get serviceAccounts"
 kubectl get serviceAccounts
+echo "kubectl config view --minify | grep namespace:"
 kubectl config view --minify | grep namespace:
+echo "kubectl config view"
 kubectl config view
