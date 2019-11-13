496f4dde7c3cf16fdcd64ac45861639115eda031
Jiayin Zhuo <jiayin.zhuo@gmail.com>
2019-11-13 09:22:43 -0500

    add update cluster

--- k8s.sh
+++ k8s.sh
@@ -28,6 +28,8 @@ ssh-keygen -y -f /home/ubuntu/jenk8ns-key-pair.pem > /home/ubuntu/.ssh/id_rsa.pu
 
 echo "Creating cluster..."
 kops create cluster --dns-zone jonathanzhuo.com --zones us-east-1a --master-size t2.medium --node-size t2.medium --name $CLUSTER_NAME --state $KOPS_STATE_STORE --ssh-public-key /home/ubuntu/.ssh/id_rsa.pub --yes
+echo "Updating cluster..."
+kops update cluster $CLUSTER_NAME --yes
 echo "************************ validate cluster **************************"
 while true; do
   kops validate cluster --name $CLUSTER_NAME | grep 'is ready' &> /dev/null
